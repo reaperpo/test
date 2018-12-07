d4e7094df12d15af228e419ed572269cf507ceb2
h <h@9.52.225.227>
2018-12-07 12:44:39 -0800

    new code

--- Codacy/src/Test.java
+++ Codacy/src/Test.java
@@ -1,6 +1,23 @@
-
+import java.lang.Integer;
 public class Test {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		String test = "HELLO";
+		if(true)
+		{
+			if(true) {
+				System.out.println("False");
+			}
+			if(true) {
+				System.out.println("False");
+			}
+			          if(true) {
+				System.out.println("False");
+			}
+try {
+	Integer.parseInt("2");
+} catch (Exception e) {
+	throw new Exception();
+}
+		}
 	}
 }
